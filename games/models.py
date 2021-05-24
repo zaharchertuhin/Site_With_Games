6a5f748d64bf02afe6e5d0c0f4a21f4d1bb8b75c
Захар <zaharchertuhin@gmail.com>
2021-05-24 21:01:53 +0400

    first

---
+++ games/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+class Game(models.Model):
+    title = models.CharField(max_length=50)
+    discription = models.TextField()
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
